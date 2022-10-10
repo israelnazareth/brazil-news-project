9081c919affde5d8e2e5b734fe9794aa4bbd35ac
Israel Nazareth <israel.fab@hotmail.com>
2022-10-10 00:41:59 -0300

    refact: remove api key

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,7 +1,7 @@
 export const fetchByCategory = async (category) => {
   try {
-    // const key = process.env.REACT_APP_API_KEY
-    const URL = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=787e45b081b0434f930304450f627e2f&country=br&category=${category}`)
+    const key = process.env.REACT_APP_API_KEY
+    const URL = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=${key}&country=br&category=${category}`)
     const objectData = await URL.json()
     return objectData.articles
   } catch (error) {
